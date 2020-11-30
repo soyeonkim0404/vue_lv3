ab2da74f558cfdd9a374d62a99db5773bd5b6d38
soyeonKim9104 <soyean7979@gmail.com>
2020-11-30 18:27:09 +0900

    [study-01] first commit

---
+++ vue-news/src/components/ToolBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+      <router-link to="/news">News</router-link> |
+      <router-link to="/ask">Ask</router-link> |
+      <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header{
+    display:flex;
+    background:pink;
+    padding:8px;
+    color:#fff;
+    font-size: 2rem;
+}
+
+.header a{
+    padding:0 15px;
+    color:#fff;
+    text-decoration: none;
+}
+
+.header .router-link-exact-active{
+    color:purple;
+}
+</style>
---
+++ vue-news/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import NewsView from '../views/NewsView.vue'
+import AskView from '../views/AskView.vue'
+import JobsView from '../views/JobsView.vue'
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: NewsView,
+        },
+        {
+            path:'/news',
+            component: NewsView,
+        },
+        {
+            path: '/ask',
+            component: AskView,
+        },
+        {
+            path: '/jobs',
+            component: JobsView,
+        }
+    ]
+})
